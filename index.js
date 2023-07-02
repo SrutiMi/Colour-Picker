08c94052f5d673ed90a8f667a391dc0a5752e44f
SrutiMi <114284489+SrutiMi@users.noreply.github.com>
2023-07-02 18:32:44 +0530

    completed the whole project

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const button=document.querySelector('.js-click');
 button.addEventListener('click',()=>{
-  console.log('Hello');
-})
+  //To change the background colour
+   document.body.style.backgroundColor=randomColour();
+  document.querySelector('.js-text').innerHTML=randomColour();
+});
+
+function randomColour(){
+   const r= Math.floor(Math.random()*256);//To generate the numbers between 0 and 255
+   const g= Math.floor(Math.random()*256);
+   const b= Math.floor(Math.random()*256);
+   const rgb= `rgb(${r},${g},${b})`;
+   return rgb;
+}
